d82285eae11fd6717a895f75fc5dbd266d784d55
S-VIN <7530023@gmail.com>
2021-06-15 22:56:53 +0300

    add saving

--- mainWindiw.h
+++ mainWindiw.h
@@ -5,23 +5,29 @@
 #include <iostream>
 #include <QTextEdit>
 #include <QDebug>
+#include <QTextDocumentWriter>
 
 class MainWindow: public QWidget {
 Q_OBJECT
 private:
     QPushButton OpenLog;
     QPushButton SaveLog;
+    QPushButton SaveAsLog;
     QTextEdit LogsEdit;
     QVBoxLayout MainInWidget;
     QHBoxLayout Instruments;
-
+    QString LogFileName;
 
     void OpenLogInit(){
-        OpenLog.setText("openLog");
+        OpenLog.setText("Open");
     };
 
     void SaveLogInit(){
-        SaveLog.setText("saveLog");
+        SaveLog.setText("Save");
+    };
+
+    void SaveAsLogInit(){
+        SaveAsLog.setText("Save as");
     };
 
     void ChooseLogInit(){
@@ -35,7 +41,7 @@ private:
     void InstrumentsInit(){
         Instruments.addWidget(&OpenLog);
         Instruments.addWidget(&SaveLog);
-
+        Instruments.addWidget(&SaveAsLog);
     };
 
     void MainInWidgetInit(){
@@ -49,6 +55,7 @@ private:
         InstrumentsInit();
         OpenLogInit();
         SaveLogInit();
+        SaveAsLogInit();
         ChooseLogInit();
         LogsEditInit();
     }
@@ -58,21 +65,45 @@ public:
         ComponentsInit();
         this->setGeometry(this->geometry().x(), this->geometry().y(),800, 800);
         QObject::connect(&OpenLog, SIGNAL(clicked(bool)), this, SLOT(ChooseLog()));
+        QObject::connect(&SaveLog, SIGNAL(clicked(bool)), this, SLOT(FileSave()));
+        QObject::connect(&SaveAsLog, SIGNAL(clicked(bool)), this, SLOT(FileSaveAs()));
     }
 
 private slots:
     void ChooseLog(){
-       QString FileName = QFileDialog::getOpenFileName(this, tr("Open log"), "/home");
-       qDebug() << FileName;
-       QFile LogFile(FileName);
+       LogFileName = QFileDialog::getOpenFileName(this, tr("Open log"), "/home");
+       QFile LogFile(LogFileName);
        LogFile.open(QIODevice::ReadWrite);
+       LogsEdit.clear();
        QTextStream in(&LogFile);
        while (!in.atEnd()) {
             QString line = in.readLine();
-            qDebug() << line;
             LogsEdit.append(line);
        }
     };
 
+    void FileSave(){
+        if (LogFileName.isEmpty())
+            return FileSaveAs();
+        if (LogFileName.startsWith(QStringLiteral(":/")))
+            return FileSaveAs();
 
+        QTextDocumentWriter writer(LogFileName);
+        bool success = writer.write(LogsEdit.document());
+        if (success) {
+            LogsEdit.document()->setModified(false);
+        }
+    }
+
+    void FileSaveAs()
+    {
+        QFileDialog fileDialog(this, tr("Save as..."));
+        fileDialog.setAcceptMode(QFileDialog::AcceptSave);
+        fileDialog.setDefaultSuffix("txt");
+        if (fileDialog.exec() != QDialog::Accepted)
+            return;
+        const QString fn = fileDialog.selectedFiles().first();
+        LogFileName = fn;
+        FileSave();
+    }
 };
