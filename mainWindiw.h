e7955fdfae5144444d04cdece1bb48dc4b1d20d8
S-VIN <7530023@gmail.com>
2021-06-15 21:09:51 +0300

    read file

--- mainWindiw.h
+++ mainWindiw.h
@@ -4,13 +4,13 @@
 #include <QVBoxLayout>
 #include <iostream>
 #include <QTextEdit>
+#include <QDebug>
 
 class MainWindow: public QWidget {
 Q_OBJECT
 private:
     QPushButton OpenLog;
     QPushButton SaveLog;
-    QFileDialog ChooseLog;
     QTextEdit LogsEdit;
     QVBoxLayout MainInWidget;
     QHBoxLayout Instruments;
@@ -29,35 +29,50 @@ private:
     };
 
     void LogsEditInit(){
-        //LogsEdit.show();
+
     };
 
     void InstrumentsInit(){
         Instruments.addWidget(&OpenLog);
         Instruments.addWidget(&SaveLog);
-        Instruments.addWidget(&ChooseLog);
+
     };
 
     void MainInWidgetInit(){
-
         MainInWidget.addLayout(&Instruments);
         MainInWidget.addWidget(&LogsEdit);
         this->setLayout(&MainInWidget);
     };
 
-public:
-    MainWindow(QWidget *parent = 0): QWidget(parent){
+    void ComponentsInit(){
         MainInWidgetInit();
         InstrumentsInit();
         OpenLogInit();
         SaveLogInit();
         ChooseLogInit();
         LogsEditInit();
-
-        QObject::connect(&OpenLog, SIGNAL(clicked(bool)), &ChooseLog, SLOT(open()));
-
     }
 
+public:
+    MainWindow(QWidget *parent = 0): QWidget(parent){
+        ComponentsInit();
+        this->setGeometry(this->geometry().x(), this->geometry().y(),800, 800);
+        QObject::connect(&OpenLog, SIGNAL(clicked(bool)), this, SLOT(ChooseLog()));
+    }
+
+private slots:
+    void ChooseLog(){
+       QString FileName = QFileDialog::getOpenFileName(this, tr("Open log"), "/home");
+       qDebug() << FileName;
+       QFile LogFile(FileName);
+       LogFile.open(QIODevice::ReadWrite);
+       QTextStream in(&LogFile);
+       while (!in.atEnd()) {
+            QString line = in.readLine();
+            qDebug() << line;
+            LogsEdit.append(line);
+       }
+    };
 
 
 };
